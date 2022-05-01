15aaff41c3cd10fef41e02dc222b25f663ed5d47
Tapdig Guliyev <tapdigguliyev@gmail.com>
2022-05-01 13:53:30 +0400

    Custom view animations built for login activity

--- app/src/main/java/com/raywenderlich/android/menagerie/ui/login/view/ProgressButton.kt
+++ app/src/main/java/com/raywenderlich/android/menagerie/ui/login/view/ProgressButton.kt
@@ -1,12 +1,16 @@
 package com.raywenderlich.android.menagerie.ui.login.view
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.Context
+import android.graphics.drawable.GradientDrawable
 import android.os.Build
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import androidx.annotation.RequiresApi
 import androidx.core.view.doOnLayout
+import androidx.core.view.updateLayoutParams
 import com.raywenderlich.android.menagerie.databinding.ViewProgressButtonBinding
 
 class ProgressButton : FrameLayout {
@@ -40,7 +44,38 @@ class ProgressButton : FrameLayout {
 
   @RequiresApi(Build.VERSION_CODES.N)
   fun transformToProgress() {
+    val drawable = binding.actionButton.background as? GradientDrawable
+    val targetWidth = targetButtonWidth
+    val targetCornerRadius = targetButtonWidth
 
+    val cornerAnimator = ValueAnimator.ofFloat(drawable?.cornerRadius ?: 0f, targetCornerRadius.toFloat())
+    val widthAnimator = ValueAnimator.ofInt(binding.actionButton.measuredWidth, targetWidth)
+    val progressBarAlphaAnimator = ObjectAnimator.ofFloat(binding.progressBar, "alpha", 0f, 1f)
+
+    cornerAnimator.duration = 1000
+    widthAnimator.duration = 1000
+    progressBarAlphaAnimator.duration = 1000
+
+    cornerAnimator.addUpdateListener {
+      drawable?.cornerRadius = it.animatedValue as Float
+
+      binding.actionButton.background = drawable
+    }
+
+    widthAnimator.addUpdateListener {
+      binding.actionButton.updateLayoutParams {
+        this.width = it.animatedValue as Int
+      }
+
+      binding.actionButton.textSize = it.animatedFraction * binding.actionButton.textSize
+    }
+
+    binding.progressBar.alpha = 0f
+    binding.progressBar.visibility = VISIBLE
+
+    cornerAnimator.start()
+    widthAnimator.start()
+    progressBarAlphaAnimator.start()
   }
 
   inline fun onClick(crossinline onClick: () -> Unit) {
